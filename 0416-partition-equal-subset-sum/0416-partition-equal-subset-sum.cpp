1c361d16d33b2c5df06cd5267715a2fdeb6ea097
Parthiv Upadhyay <parthivu1309@gmail.com>
2026-06-24 19:20:39 +0530

    Time: 109 ms (68.02%), Space: 16.9 MB (70.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -14,34 +14,34 @@ public:
         int target = sum / 2;
         
         // Changed to vector<bool> initialized to false for cleaner tabulation
-        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+        vector<bool>prev(target + 1, false);
+        prev[0] = true;
         
-        // Base case 1: Target 0 is always achievable (empty subset)
-        for(int i = 0; i < n; i++){
-            dp[i][0] = true;
-        }
         
         // Base case 2: First element can achieve a target equal to its value
         if(target >= nums[0]){
-            dp[0][nums[0]] = true;
+            prev[nums[0]] = true;
         }
         
         for(int i = 1; i < n; i++){
+            vector<bool>curr(target + 1, false);
+            curr[0] = true;
             for(int j = 1; j <= target; j++){
 
                 bool take = false;
                 if(j >= nums[i]) {
-                    take = dp[i-1][j - nums[i]];
+                    take = prev[j - nums[i]];
                 }
 
                 // FIX 1: Look at the previous row
-                bool notTake = dp[i-1][j]; 
+                bool notTake = prev[j]; 
                 
                 // FIX 2: Store the result of (take OR notTake) into the current state
-                dp[i][j] = take || notTake; 
+                curr[j] = take || notTake; 
             }
+            prev = curr;
         }
         
-        return dp[n-1][target];
+        return prev[target];
     }
 };
