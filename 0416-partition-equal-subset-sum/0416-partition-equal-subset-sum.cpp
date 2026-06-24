2719246cc0c17c9e79e16440638732d76b662f0d
Parthiv Upadhyay <parthivu1309@gmail.com>
2026-06-24 19:13:50 +0530

    Time: 174 ms (20%), Space: 17.3 MB (69.23%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
-    bool helper(int ind, int target, vector<int>& nums, vector<vector<int>>& dp){
-        if(target == 0) return true;
-        if(ind == 0) return (nums[ind] == target);
-
-        if(dp[ind][target] != -1)return dp[ind][target];
-
-        bool take = false;
-        if(target >= nums[ind]) take = helper(ind-1, target - nums[ind], nums, dp);
-
-        bool notTake = helper(ind-1, target, nums, dp);
-
-        return dp[ind][target] = (take || notTake);
-    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
+        
         for(int i = 0; i < n; i++){
             sum += nums[i];
         }
 
-        vector<vector<int>>dp(n, vector<int>(sum/2 + 1, -1));
+        // If the sum is odd, we cannot divide it into two equal integer halves
+        if(sum % 2 != 0) return false;
+        
+        int target = sum / 2;
+        
+        // Changed to vector<bool> initialized to false for cleaner tabulation
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+        
+        // Base case 1: Target 0 is always achievable (empty subset)
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+        
+        // Base case 2: First element can achieve a target equal to its value
+        if(target >= nums[0]){
+            dp[0][nums[0]] = true;
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= target; j++){
 
-        if(sum % 2) return false;
-        else return helper(n-1, sum/2, nums, dp);
+                bool take = false;
+                if(j >= nums[i]) {
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                // FIX 1: Look at the previous row
+                bool notTake = dp[i-1][j]; 
+                
+                // FIX 2: Store the result of (take OR notTake) into the current state
+                dp[i][j] = take || notTake; 
+            }
+        }
+        
+        return dp[n-1][target];
     }
 };
