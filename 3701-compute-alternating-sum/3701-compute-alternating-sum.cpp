08564319bce4ace0ef5cf6bce282f35cba34e59a
Parthiv Upadhyay <parthivu1309@gmail.com>
2026-07-18 01:45:14 +0530

    Time: 0 ms (100%), Space: 32.6 MB (10.42%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int sum =0;
+        int i=0;
+        while(i!=nums.size()){
+            if(i%2==0){
+                sum+=nums[i];
+            }
+            else{
+                sum-=nums[i];
+            }
+
+            i++;
+        }
+
+        return sum;
+    }
+};
