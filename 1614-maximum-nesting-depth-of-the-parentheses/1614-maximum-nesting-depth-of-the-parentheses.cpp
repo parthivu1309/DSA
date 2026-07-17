fc584cab83c58597b9440383dc4cd30aca2bad2d
Parthiv Upadhyay <parthivu1309@gmail.com>
2026-07-18 01:42:10 +0530

    Time: 2 ms (4.18%), Space: 8.4 MB (61.23%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0;
+        int maxDepth = 0;
+
+        for(char c : s){
+            if(c == '('){
+                depth++;
+                maxDepth = max(maxDepth, depth);
+            }
+            else if(c == ')'){
+                depth--;
+            }
+        }
+
+        return maxDepth;
+    }
+};
