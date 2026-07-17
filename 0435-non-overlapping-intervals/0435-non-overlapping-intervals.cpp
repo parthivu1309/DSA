e2e931616caf13122bdf8ad765e566f7bee83bc7
Parthiv Upadhyay <parthivu1309@gmail.com>
2026-07-18 01:32:09 +0530

    Time: 59 ms (37.14%), Space: 94 MB (39.66%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+
+    int count = 0;
+    int prevEnd = intervals[0][1];
+
+    for(int i = 1; i < intervals.size(); i++){
+        if(intervals[i][0] < prevEnd){
+            count++;
+            prevEnd = min(prevEnd, intervals[i][1]); // 🔥 KEY FIX
+        } else {
+            prevEnd = intervals[i][1];
+        }
+    }
+
+    return count;
+}
+};
